0e8d6a24e9730ee0b37688be712b41f17159a85f
ASHIN <ashin.ashin007@gmail.com>
2019-03-25 15:39:16 +0530

    feat: Grid

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import './App.scss';
 
 const Home = () => {
       return (
-            <div>
-                  <p>Home PAge</p>
+            <div class="grid">
+                  <div class="grid__item grid__item--xs-span-1">xs-1</div>
+                  <div class="grid__item grid__item--xs-span-2">xs-2</div>
+                  <div class="grid__item grid__item--xs-span-3">xs-3</div>
+                  <div class="grid__item grid__item--xs-span-6">xs-6</div>
+
+                  <div class="grid__item grid__item--md-span-4">md-4</div>
+                  <div class="grid__item grid__item--md-span-4">md-4</div>
+                  <div class="grid__item grid__item--md-span-4">md-4</div>
+
+                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
+                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
+                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
+                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
+                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
+                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
+                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
+                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
+                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
+                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
+                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
+                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
             </div>
       );
 };
