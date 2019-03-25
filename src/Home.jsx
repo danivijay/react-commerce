bc7d833c56d40b5d5fcd8b78cfafb507097584a5
ASHIN <ashin.ashin007@gmail.com>
2019-03-25 15:45:19 +0530

    feat: grid responsive

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,27 +4,9 @@ import './App.scss';
 const Home = () => {
       return (
             <div class="grid">
-                  <div class="grid__item grid__item--xs-span-1">xs-1</div>
-                  <div class="grid__item grid__item--xs-span-2">xs-2</div>
-                  <div class="grid__item grid__item--xs-span-3">xs-3</div>
-                  <div class="grid__item grid__item--xs-span-6">xs-6</div>
-
-                  <div class="grid__item grid__item--md-span-4">md-4</div>
-                  <div class="grid__item grid__item--md-span-4">md-4</div>
-                  <div class="grid__item grid__item--md-span-4">md-4</div>
-
-                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
-                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
-                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
-                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
-                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
-                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
-                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
-                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
-                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
-                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
-                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
-                  <div class="grid__item grid__item--sm-span-1">sm-1</div>
+                  <div class="grid__item grid__item--sm-span-4">sm-4-1</div>
+                  <div class="grid__item grid__item--sm-span-4">sm-4-2</div>
+                  <div class="grid__item grid__item--sm-span-4">sm-4-3</div>
             </div>
       );
 };
