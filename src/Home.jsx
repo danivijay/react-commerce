c209cffebd5ff28e477417c2063f35c9627f4978
ASHIN <ashin.ashin007@gmail.com>
2019-03-25 15:11:55 +0530

    feat: init added router to home page

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Home = () => {
+      return (
+            <div>
+                  <p>Home PAge</p>
+            </div>
+      );
+};
+
+export default Home;
