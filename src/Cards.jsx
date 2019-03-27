9e1a30c08ce1c77b3db42b8c0d7a1e495e8569e4
danivivriti <danimvijay@gmail.com>
2019-03-27 20:32:36 +0530

    feat: convert to black and white

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -2,27 +2,30 @@ import React from 'react';
 import './Cards.scss';
 
 const Cards = () => {
-      return (
-            <div class="cards">
-                  <article class="card">
-                        <div class="card__top">
-                              <img src="https://picsum.photos/250/200?random" alt="" />
-                        </div>
-                        <div class="card__bottom">
-                              <h1 class="card__heading">Item name</h1>
-                              <p>item description</p>
-                        </div>
-                        <div class="card__footer">
-                              <a href="/" class="btn btn--pad-20 btn--primary">
-                                    Regular button
-                              </a>
-                              <a href="/" class="btn btn--pad-20 btn--uppercase btn--secondary">
-                                    Buy now
-                              </a>
-                        </div>
-                  </article>
-            </div>
-      );
+    return (
+        <div class="cards">
+            <article class="card">
+                <div class="card__top">
+                    <img src="https://picsum.photos/250/200?random" alt="" />
+                </div>
+                <div class="card__bottom">
+                    <h1 class="card__heading">Item name</h1>
+                    <p>item description</p>
+                </div>
+                <div class="card__footer">
+                    <a href="/" class="btn btn--pad-20 btn--primary">
+                        Regular button
+                    </a>
+                    <a
+                        href="/"
+                        class="btn btn--pad-20 btn--uppercase btn--secondary"
+                    >
+                        Buy now
+                    </a>
+                </div>
+            </article>
+        </div>
+    );
 };
 
 export default Cards;
