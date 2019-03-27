efe1b1c1624609bae0507e0f680339079ade31eb
ASHIN <ashin.ashin007@gmail.com>
2019-03-27 15:48:55 +0530

    feat: Header and Footer

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.scss';
 
-const Home = () => {
+const Cart = () => {
       return (
             <div class="grid">
-                  <div class="grid__item grid__item--sm-span-4">sm-4-1</div>
+                  <div class="grid__item grid__item--sm-span-4">cart</div>
                   <div class="grid__item grid__item--sm-span-4">sm-4-2</div>
                   <div class="grid__item grid__item--sm-span-4">sm-4-3</div>
             </div>
       );
 };
 
-export default Home;
+export default Cart;
