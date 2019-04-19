eae3a5a9db5484e3faadc95e3f37f4d9deeb2d86
ashinpchandran <ashin.ashin007@gmail.com>
2019-04-19 16:41:25 +0530

    feat(admin): Added product table

--- src/Components/Pages/AdminPanel.jsx
+++ src/Components/Pages/AdminPanel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './AdminPanel.scss';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
@@ -10,45 +10,116 @@ const GET_TRANSACTIONS = gql`
             quantity
             user_id
             date
-            address_id
+            product_id
             currency
             status
         }
     }
 `;
 
+const GET_PRODUCTS = gql`
+    {
+        products {
+            id
+            price
+            stock
+            name
+        }
+    }
+`;
+
+const GET_USER_NAME = gql`
+    {
+        user(id: "5cb9714e34a03d20b0d6ff1d") {
+            userName
+        }
+    }
+`;
+
 const AdminPanel = () => {
     return (
-        <Query query={GET_TRANSACTIONS}>
-            {({ data }) =>
-                console.log(data) || (
-                    <table id="admintable">
-                        <tr>
-                            <th>Name</th>
-                            <th>Address</th>
-                            <th>Quantity</th>
-                            <th>Date</th>
-                            <th>Price</th>
-                            <th>currency</th>
-                            <th>Status</th>
-                        </tr>
-                        {data &&
-                            data.transactions &&
-                            data.transactions.map((transaction) => (
-                                <tr>
-                                    <td>{transaction.user_id}</td>
-                                    <td>{transaction.address_id}</td>
-                                    <td>{transaction.quantity}</td>
-                                    <td>{transaction.date}</td>
-                                    <td>{transaction.price}</td>
-                                    <td>{transaction.currency}</td>
-                                    <td>{transaction.status}</td>
-                                </tr>
-                            ))}
-                    </table>
-                )
-            }
-        </Query>
+        <Fragment>
+            <Query query={GET_TRANSACTIONS}>
+                {({ data }) =>
+                    console.log(data) || (
+                        <Fragment>
+                            <h1>Transactions</h1>
+                            <table className="admintable">
+                                <tbody>
+                                    <tr>
+                                        <th>Transaction ID</th>
+                                        <th>User ID</th>
+                                        <th>User Name</th>
+                                        <th>Product</th>
+                                        <th>Product ID</th>
+                                        <th>Address</th>
+                                        <th>Quantity</th>
+                                        <th>Date</th>
+                                        <th>Price</th>
+                                        <th>currency</th>
+                                        <th>Status</th>
+                                    </tr>
+                                    {data &&
+                                        data.transactions &&
+                                        data.transactions.map((transaction) => (
+                                            <tr>
+                                                <td>{transaction.id}</td>
+                                                <td>{transaction.user_id}</td>
+                                                <td>userName</td>
+                                                <td>productname</td>
+                                                <td>
+                                                    {transaction.product_id}
+                                                </td>
+                                                <td>Address</td>
+                                                <td>{transaction.quantity}</td>
+                                                <td>{transaction.date}</td>
+                                                <td>price</td>
+                                                <td>{transaction.currency}</td>
+                                                <td>{transaction.status}</td>
+                                            </tr>
+                                        ))}
+                                </tbody>
+                            </table>
+                        </Fragment>
+                    )
+                }
+            </Query>
+
+            <Query query={GET_PRODUCTS}>
+                {({ data }) =>
+                    console.log(data) || (
+                        <Fragment>
+                            <h1>Products</h1>
+                            <table className="admintable">
+                                <tbody>
+                                    <tr>
+                                        <th>Product Name</th>
+                                        <th>Product ID</th>
+                                        <th>Price</th>
+                                        <th>Stock</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                    {data &&
+                                        data.products &&
+                                        data.products.map((product) => (
+                                            <tr>
+                                                <td>{product.name}</td>
+                                                <td>{product.id}</td>
+                                                <td>{product.price}</td>
+                                                <td>{product.stock}</td>
+                                                <td>
+                                                    <button>Add</button>
+                                                    <button>Delete</button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                </tbody>
+                            </table>
+                        </Fragment>
+                    )
+                }
+            </Query>
+        </Fragment>
     );
 };
 
